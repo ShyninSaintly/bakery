0bb5cf5bd6d57074f0a4c47fe9fa3a121d078627
Gleb <mamaka.s.pistoletom@gmail.com>
2025-10-30 17:01:10 +0300

    second init commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React, {StrictMode} from "react";
+import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import App from 'app/App.tsx';
+import './index.css';
 
-// @ts-ignore
-ReactDOM.render(
-    <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
         <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-)
+    </StrictMode>,
+);
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,14 +1,19 @@
-import {Form,Button, FieldError, Input, Label, TextField} from "react-aria-components";
+// @ts-ignore
+import {Button, Form, Input, Label, TextField} from "react-aria-components";
 
 export const LoginPage = () => {
     return (
         <Form>
-            <TextField name="email" type="email" isRequired>
-                <Label>Email</Label>
-                <Input />
-                <FieldError />
+            <TextField name='username' type='text' isRequired>
+                <Label>Логин</Label>
+                <Input type='text' placeholder='Введите логин'  />
             </TextField>
-            <Button type="submit">Submit</Button>
+            <TextField name='password' type='password' isRequired>
+                <Label>Пароль</Label>
+                <Input type='password' placeholder='Введите пароль'/>
+            </TextField>
+            <Button type='button'>Регистрация</Button>
+            <Button type='submit'>Войти</Button>
         </Form>
     );
 };
