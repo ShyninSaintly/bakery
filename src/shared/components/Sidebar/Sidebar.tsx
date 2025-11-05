b39c3d7b8aeb67a14e084a898813074fd51c0c2b
Gleb <mamaka.s.pistoletom@gmail.com>
2025-11-05 13:35:43 +0300

    working maps

--- src/shared/components/Sidebar/Sidebar.tsx
+++ src/shared/components/Sidebar/Sidebar.tsx
@@ -1,23 +1,37 @@
-import {Menu, MenuTrigger} from "react-aria-components";
-import {useState} from "react";
+import {Grid, Item, Menu, Provider, View} from "@adobe/react-spectrum";
+import {Link} from "react-router";
 
-const Sidebar = () => {
-    const [selected,setSelected]=useState<Selection>(
-        new Set(['Sidebar','Console'])
-    );
+
+export const Sidebar = () => {
     return (
-        <MenuTrigger>
-            <ActionButton>Открыть</ActionButton>
-            <Menu selectionMode={"single"} selectedKeys={selected} onSelectionChange={setSelected}>
-                <Item href="/main">Элемент 1</Item>
-                <Item href="/main">Элемент 2</Item>
-                <Item href="/main">Элемент 3</Item>
-                <Item href="/main">Элемент 4</Item>
-                <Item href="/main">Элемент 5</Item>
-                <Item href="/main">Элемент 6</Item>
-            </Menu>
-        </MenuTrigger>
+        <Provider>
+            <Grid areas={['header header', 'sidebar content', 'footer footer']}
+                columns={['200px','1fr']}
+                rows={['auto','1fr','auto']}
+                height='100vh'>
+                <View gridArea='header'>HederSidebar</View>
+                <View gridArea='sidebar'>
+                    <Menu onAction={(key)=>console.log(key)}>
+                        <Item>
+                            <Link href={"#"}>
+                                <Text>Домоэ</Text>
+                            </Link>
+                        </Item>
+                        <Item>
+                            <Link href="#">
+                                <Text>Домоэ</Text>
+                            </Link>
+                        </Item>
+                        <Item>
+                            <Link href="#">
+                                <Text>Домоэ</Text>
+                            </Link>
+                        </Item>
+                    </Menu>
+                </View>
+                <View gridArea='content'>Main Content</View>
+                <View gridArea='footer'>Footer Content</View>
+            </Grid>
+        </Provider>
     );
 };
-
-export default Sidebar;
